ae8fb2c0c1b62251a9d1c8583ce486f3aaafa5ef
Mikel Arnaiz <mikel.arnaiz@philips.com>
2020-11-06 14:55:28 +0100

    adds get data request

---
+++ src/components/App/App.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+export const App = () => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    try {
+      const getData = async () => {
+        const response = await fetch('https://codestar-iot-api.herokuapp.com/get')
+        const json = await response.json()
+        setData(json)
+      }
+      
+      getData()
+    } catch (err) {
+      console.error()
+    }
+  }, [])
+
+  return (
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>No data</p>}
+    </div>
+  );
+}
