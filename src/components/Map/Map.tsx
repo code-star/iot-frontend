d86896fc7284c1bef1b30468fd0ba148428b0e96
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2020-12-23 15:15:44 +0100

    shows sensor details

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react'
 
 import ReactMapGL, { Marker } from 'react-map-gl'
 import { Sensor } from '../../API/Sensor/Sensor'
+import styles from './Map.module.scss'
 
 const TOKEN = 'pk.eyJ1IjoiY29kZXN0YXItaW90IiwiYSI6ImNraDZjeTRjcTA5Z2Eyd281djNnc2d3eHUifQ.VcY4aGyFHBQo_r701-3A_A'
 
@@ -43,16 +44,62 @@ const getFirstSensorData = (sensors: Sensor[]): MapViewport => {
   }
 }
 
+type SensorData = Sensor &
+  Readonly<{
+    showDetails: boolean
+  }>
+
+function mapSensorsData(sensors: Sensor[]) {
+  console.log(sensors)
+  return sensors.reduce<Record<string, SensorData>>(
+    (acc, curr, index) => ({
+      ...acc,
+      [index.toString()]: {
+        ...curr,
+        showDetails: false,
+      },
+    }),
+    {},
+  )
+}
+
 const Markers: FC<MapProps> = ({ sensorsData }) => {
+  const [data, setData] = useState<Record<string, SensorData>>(() => mapSensorsData(sensorsData))
+
+  const toggleShowDetails = (index: string) => () => {
+    setData((data) => ({
+      ...data,
+      [index]: {
+        ...data[index],
+        showDetails: !data[index].showDetails,
+      },
+    }))
+  }
+
   return (
     <>
-      {sensorsData
-        .filter((entry) => entry.coordinates[0] > 0)
-        .map(({ coordinates: [lat, lng] }, index) => (
-          <Marker key={index} longitude={lng} latitude={lat}>
-            x
-          </Marker>
-        ))}
+      {Object.entries(data)
+        .filter(([, sensorData]) => sensorData.coordinates[0] > 0)
+        .map(([index, sensorData]) => {
+          const {
+            showDetails,
+            temperature,
+            coordinates: [lat, lng],
+          } = sensorData
+
+          return (
+            <Marker key={index} className={styles.marker} longitude={lng} latitude={lat}>
+              <span onClick={toggleShowDetails(index)}>
+                <span>📍</span>
+                {showDetails ? (
+                  <div className={styles.details}>
+                    <p>Temperature: {temperature}</p>
+                  </div>
+                ) : null}
+              </span>
+            </Marker>
+          )
+        })}
     </>
   )
 }
